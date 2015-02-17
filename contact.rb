07356dc7c5700dff4e43d506222c5e8c9411d7e6
drewhgood <drewhgood@gmail.com>
2015-02-17 20:13:44 +0000

    successfull contact with database established. Can itterate over records.

--- contact.rb
+++ contact.rb
@@ -1,11 +1,33 @@
+require 'pg'
+
 class Contact
 
+  def initialize
+  end
 
-def self.connection
-  p 'test'
+  def self.connection
+    return @conn if @conn
+    @conn = PG.connect( 
+      dbname: 'dug9nq59bmjh0',
+      host: 'ec2-107-21-93-97.compute-1.amazonaws.com',
+      port: 5432,
+      user: 'thnwitebsqrgsl',
+      password:'ARITSGAGYX08xeuAk9mZR6pXMy'
+    )
+  end
+
+  # def self.test
+  #   connection.exec("SELECT * FROM contacts;") do |contacts|
+  #     contacts.each do |contact|
+  #       p contact
+  #     end
+  #   end
+  # end
+
 end
 
-end
 
 
-Contact.connection
+
+
+p Contact.test
