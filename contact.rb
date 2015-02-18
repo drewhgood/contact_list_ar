530a5a3a9b61c5a23293a19bc20111b99db3d3c8
drewhgood <drewhgood@gmail.com>
2015-02-18 03:08:27 +0000

    cannot add duplicate records based on email.

--- contact.rb
+++ contact.rb
@@ -24,17 +24,41 @@ class Contact
     )
   end
 
+
   def save
-    self.id ? update : create
+    self.id ? update : create_contact  
   end
 
-  def create
-    self.class.connection.exec_params("INSERT INTO contacts (firstname, lastname, email)
-    VALUES ($1,$2,$3)",[@firstname, @lastname, @email])
+
+  def create_contact
+      
+      matching_records = self.class.connection.exec_params("SELECT * FROM contacts WHERE email = $1;",[@email])
+      
+      if matching_records.ntuples!= 0
+      puts "Email address already exists."
+
+      else 
+      self.class.connection.exec_params("INSERT INTO contacts (firstname, lastname, email)
+      VALUES ($1,$2,$3)",[@firstname, @lastname, @email]) 
+
+      id = self.class.connection.exec_params("SELECT id FROM contacts 
+        WHERE firstname = $1 AND lastname = $2 AND email = $3",[@firstname, @lastname, @email])
+      
+      id.each do |item|
+        item
+        @id = item["id"] 
+
+      end
+
+    end
+
   end
 
   def update
-    puts 'update'
+    puts "Updated"
+    # self.class.connection.exec_params("UPDATE pets SET name = $1 WHERE id = $2", [@name, @id])
+    #once we have the update method we can complete this
+    # self.class.connection.exec_params("UPDATE contacts SET name = $1 WHERE id = $2", [@name, @id])
   end
 
   def self.test
@@ -47,7 +71,7 @@ class Contact
 
 end
 
-p c = Contact.new('Bob','Marley','Marley@gmail.com')
+p c = Contact.new('Jose','Pannn','joese@hotmail.com')
 c.save
-print Contact.test
+# print Contact.test
 
