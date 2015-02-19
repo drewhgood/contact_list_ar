de737d8c8b38955aaf369c870b240fe3e747f6b9
drewhgood <drewhgood@gmail.com>
2015-02-19 00:19:40 +0000

    search by last name added.

--- application.rb
+++ application.rb
@@ -11,11 +11,17 @@ class Application
    puts "#{contact.firstname} #{contact.lastname} has been deleted."
   end
 
+  def self.find_by_lastname(lastname)
+   contact =  Contact.find_by(lastname: lastname)
+   puts "#{contact.firstname} #{contact.lastname}  -  Email:#{contact.email}"
+  end
+
+
 end
 
 
 
-
+Application.find_by_lastname('Good')
 #Application.add_contact('andrew','good','drew@dsa.com')
 
 # Application.destroy_contact(4)
