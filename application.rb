38a32fe56beeb719d769154a9b1f64b068bbdb9d
drewhgood <drewhgood@gmail.com>
2015-02-19 00:10:38 +0000

    can add and destory contacts

--- application.rb
+++ application.rb
@@ -6,9 +6,17 @@ class Application
     Contact.create(firstname: firstname, lastname: lastname, email: email)
   end
 
+  def self.destroy_contact(id)
+   contact =  Contact.find(id).destroy
+   puts "#{contact.firstname} #{contact.lastname} has been deleted."
+  end
+
 end
 
 
 
 
-Application.add_contact('Andrew', 'Good', 'drewhgood@gmail.com')
+#Application.add_contact('andrew','good','drew@dsa.com')
+
+# Application.destroy_contact(4)
+
--- contact.rb
+++ contact.rb
@@ -1,8 +1,8 @@
 require_relative 'setup'
 
 class Contact < ActiveRecord::Base
-
   validates :lastname, presence: true
   validates :firstname, presence: true
   validates :email, presence: true
+
 end
